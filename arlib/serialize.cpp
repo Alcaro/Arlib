7a68ef6442e0336c63118d247b7d929938750752
Alcaro <floating@muncher.se>
2016-08-03 01:00:18 +0200

    This is insane.

--- arlib/serialize.cpp
+++ arlib/serialize.cpp
@@ -2,41 +2,26 @@
 #include "test.h"
 #include <string.h>
 
+/*
+
+{ a; b; }
+a=1
+b=2
+
+{ a { b; c; } d; }
+a
+  b=1
+  c=2
+d=3
+
+*/
+
 #ifdef ARLIB_TEST
-struct serializable_test {
-int a;
-int b;
-
-onserialize() {
-	SER(a);
-	SER(b) SER_HEX;
-}
-};
-
-class serializer_test : public serializer_base<serializer_test> {
-public:
-	int phase;
-	
-	template<typename T>
-	void serialize(const char * name, T& member, const serialize_opts& opts)
-	{
-		if(0);
-		else if (phase==0 && !strcmp(name, "a") && member==16 && opts.hex==false) phase++;
-		else if (phase==1 && !strcmp(name, "b") && member==32 && opts.hex==true) phase++;
-		else phase=-1;
-		member++;
-	}
-};
+#include "bml.h"
 
 test()
 {
-	serializer_test s;
-	s.phase = 0;
-	serializable_test item;
-	item.a = 16;
-	item.b = 32;
-	item.serialize(s);
 	
-	return s.phase==2 && item.a==17 && item.b==33;
+	return true;
 }
 #endif
