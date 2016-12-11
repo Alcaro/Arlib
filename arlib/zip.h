a9812ed9fa5bc76366d8ad3608390c00796610aa
Alcaro <floating@muncher.se>
2016-12-11 03:15:14 +0100

    Fill in this ZIP parser

--- arlib/zip.h
+++ arlib/zip.h
@@ -2,18 +2,4 @@
 #include "array.h"
 #include "file.h"
 
-class zipfile : nocopy {
-	class impl;
-public:
-	static zipfile* create(file* core);
-	static zipfile* create(filewrite* core);
-	
-	virtual arrayview<string> files() = 0;
-	virtual array<byte> read(cstring name) = 0;
-	
-	//Only usable if the zipfile was created with a filewrite.
-	//Writing a blank array deletes the file.
-	virtual void write(cstring name, arrayview<byte> data) = 0;
-	
-	virtual ~zipfile() {}
-};
+//TODO: parser doesn't reject bad zips
