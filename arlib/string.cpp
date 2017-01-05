9bb2a5bb9fcd92547ca0bdf589f18bb527828d40
Alcaro <floating@muncher.se>
2017-01-05 03:51:07 +0100

    Reformat this

--- arlib/string.cpp
+++ arlib/string.cpp
@@ -33,258 +33,261 @@ uint8_t* string::alloc(uint8_t* prev, uint32_t prevsize, uint32_t newsize)
 	*(int*)ptr = 1;
 	return ptr+sizeof(int);
 }
-	void string::unshare() const
+
+void string::unshare() const
+{
+	if (inlined()) return;
+	if (m_owning && *(int*)(m_data-sizeof(int))==1) return;
+	
+	uint8_t* prevdat = m_data;
+	m_data = alloc(NULL,0, m_len);
+	memcpy(m_data, prevdat, m_len);
+	m_data[m_len] = '\0';
+	
+	if (m_owning) alloc(prevdat,m_len, 0);
+	
+	m_owning = true;
+	m_nul = true;
+}
+
+void string::resize(uint32_t newlen)
+{
+	unshare();
+	
+	switch (!inlined()<<1 | (newlen>max_inline))
 	{
-		if (inlined()) return;
-		if (m_owning && *(int*)(m_data-sizeof(int))==1) return;
+	case 0: // small->small
+		{
+			m_inline[newlen] = '\0';
+			m_inline_len = max_inline-newlen;
+		}
+		break;
+	case 1: // small->big
+		{
+			uint8_t* newptr = alloc(NULL,0, newlen);
+			memcpy(newptr, m_inline, max_inline);
+			newptr[newlen] = '\0';
+			m_data = newptr;
+			m_len = newlen;
+			m_owning = true;
+			m_nul = true;
+			
+			m_inline_len = -1;
+		}
+		break;
+	case 2: // big->small
+		{
+			uint8_t* oldptr = m_data;
+			uint32_t oldlen = m_len;
+			memcpy(m_inline, oldptr, newlen);
+			alloc(oldptr,oldlen, 0);
+			m_inline[newlen] = '\0';
+			m_inline_len = max_inline-newlen;
+		}
+		break;
+	case 3: // big->big
+		{
+			m_data = alloc(m_data,m_len, newlen);
+			m_data[newlen] = '\0';
+			m_len = newlen;
+		}
+		break;
+	}
+}
+
+void string::init_from(arrayview<byte> data)
+{
+	const uint8_t * str = data.ptr();
+	uint32_t len = data.size();
+	
+	if (len <= max_inline)
+	{
+		memcpy(m_inline, str, len);
+		m_inline[len] = '\0';
+		m_inline_len = max_inline-len;
+	}
+	else
+	{
+		m_inline_len = -1;
 		
-		uint8_t* prevdat = m_data;
-		m_data = alloc(NULL,0, m_len);
-		memcpy(m_data, prevdat, m_len);
-		m_data[m_len] = '\0';
+		m_data = alloc(NULL,0, len+1);
+		memcpy(m_data, str, len);
+		m_data[len]='\0';
 		
-		if (m_owning) alloc(prevdat,m_len, 0);
-		
+		m_len = len;
 		m_owning = true;
 		m_nul = true;
 	}
+}
+
+void string::init_from_nocopy(arrayview<byte> data)
+{
+	const uint8_t * str = data.ptr();
+	uint32_t len = data.size();
 	
-	void string::resize(uint32_t newlen)
+	if (len <= max_inline)
+	{
+		memcpy(m_inline, str, len);
+		m_inline[len] = '\0';
+		m_inline_len = max_inline-len;
+	}
+	else
+	{
+		m_inline_len = -1;
+		
+		m_data = (uint8_t*)str; // if m_owning is false, we know to not modify this
+		m_len = len;
+		m_owning = false;
+		m_nul = false;
+	}
+}
+
+void string::replace(int32_t pos, int32_t len, const string& newdat)
+{
+	//if newdat is a cstring backed by this, then modifying this invalidates that string, so it's illegal
+	//if newdat equals this, then the memmoves will mess things up
+	if (this == &newdat)
+	{
+		string copy = newdat;
+		replace(pos, len, copy);
+		return;
+	}
+	
+	uint32_t prevlength = length();
+	uint32_t newlength = newdat.length();
+	
+	if (newlength < prevlength)
 	{
 		unshare();
-		
-		switch (!inlined()<<1 | (newlen>max_inline))
-		{
-		case 0: // small->small
-			{
-				m_inline[newlen] = '\0';
-				m_inline_len = max_inline-newlen;
-			}
-			break;
-		case 1: // small->big
-			{
-				uint8_t* newptr = alloc(NULL,0, newlen);
-				memcpy(newptr, m_inline, max_inline);
-				newptr[newlen] = '\0';
-				m_data = newptr;
-				m_len = newlen;
-				m_owning = true;
-				m_nul = true;
-				
-				m_inline_len = -1;
-			}
-			break;
-		case 2: // big->small
-			{
-				uint8_t* oldptr = m_data;
-				uint32_t oldlen = m_len;
-				memcpy(m_inline, oldptr, newlen);
-				alloc(oldptr,oldlen, 0);
-				m_inline[newlen] = '\0';
-				m_inline_len = max_inline-newlen;
-			}
-			break;
-		case 3: // big->big
-			{
-				m_data = alloc(m_data,m_len, newlen);
-				m_data[newlen] = '\0';
-				m_len = newlen;
-			}
-			break;
-		}
+		memmove(ptr()+pos+newlength, ptr()+pos+len, prevlength-len-pos);
+		resize(prevlength - len + newlength);
+	}
+	if (newlength == prevlength)
+	{
+		unshare();
+	}
+	if (newlength > prevlength)
+	{
+		resize(prevlength - len + newlength);
+		memmove(ptr()+pos+newlength, ptr()+pos+len, prevlength-len-pos);
 	}
 	
-	void string::init_from(arrayview<byte> data)
-	{
-		const uint8_t * str = data.ptr();
-		uint32_t len = data.size();
-		
-		if (len <= max_inline)
-		{
-			memcpy(m_inline, str, len);
-			m_inline[len] = '\0';
-			m_inline_len = max_inline-len;
-		}
-		else
-		{
-			m_inline_len = -1;
-			
-			m_data = alloc(NULL,0, len+1);
-			memcpy(m_data, str, len);
-			m_data[len]='\0';
-			
-			m_len = len;
-			m_owning = true;
-			m_nul = true;
-		}
-	}
-	void string::init_from_nocopy(arrayview<byte> data)
-	{
-		const uint8_t * str = data.ptr();
-		uint32_t len = data.size();
-		
-		if (len <= max_inline)
-		{
-			memcpy(m_inline, str, len);
-			m_inline[len] = '\0';
-			m_inline_len = max_inline-len;
-		}
-		else
-		{
-			m_inline_len = -1;
-			
-			m_data = (uint8_t*)str; // if m_owning is false, we know to not modify this
-			m_len = len;
-			m_owning = false;
-			m_nul = false;
-		}
-	}
-	void string::replace(int32_t pos, int32_t len, const string& newdat)
-	{
-		//if newdat is a cstring backed by this, then modifying this invalidates that string, so it's illegal
-		//if newdat equals this, then the memmoves will mess things up
-		if (this == &newdat)
-		{
-			string copy = newdat;
-			replace(pos, len, copy);
-			return;
-		}
-		
-		uint32_t prevlength = length();
-		uint32_t newlength = newdat.length();
-		
-		if (newlength < prevlength)
-		{
-			unshare();
-			memmove(ptr()+pos+newlength, ptr()+pos+len, prevlength-len-pos);
-			resize(prevlength - len + newlength);
-		}
-		if (newlength == prevlength)
-		{
-			unshare();
-		}
-		if (newlength > prevlength)
-		{
-			resize(prevlength - len + newlength);
-			memmove(ptr()+pos+newlength, ptr()+pos+len, prevlength-len-pos);
-		}
-		
-		memcpy(ptr()+pos, newdat.ptr(), newlength);
-	}
+	memcpy(ptr()+pos, newdat.ptr(), newlength);
+}
+
+string string::replace(const string& in, const string& out)
+{
+	size_t outlen = length();
 	
-	string string::replace(const string& in, const string& out)
+	if (in.length() != out.length())
 	{
-		size_t outlen = length();
-		
-		if (in.length() != out.length())
-		{
-			uint8_t* haystack = ptr();
-			uint8_t* haystackend = ptr()+length();
-			while (true)
-			{
-				haystack = (uint8_t*)memmem(haystack, haystackend-haystack, in.ptr(), in.length());
-				if (!haystack) break;
-				
-				haystack += in.length();
-				outlen += out.length(); // outlen-inlen is type uint - bad idea
-				outlen -= in.length();
-			}
-		}
-		
-		string ret;
-		uint8_t* retptr = ret.construct(outlen).ptr();
-		
-		uint8_t* prev = ptr();
-		uint8_t* myend = ptr()+length();
+		uint8_t* haystack = ptr();
+		uint8_t* haystackend = ptr()+length();
 		while (true)
 		{
-			uint8_t* match = (uint8_t*)memmem(prev, myend-prev, in.ptr(), in.length());
-			if (!match) break;
+			haystack = (uint8_t*)memmem(haystack, haystackend-haystack, in.ptr(), in.length());
+			if (!haystack) break;
 			
-			memcpy(retptr, prev, match-prev);
-			retptr += match-prev;
-			prev = match + in.length();
-			
-			memcpy(retptr, out.ptr(), out.length());
-			retptr += out.length();
+			haystack += in.length();
+			outlen += out.length(); // outlen-inlen is type uint - bad idea
+			outlen -= in.length();
 		}
-		memcpy(retptr, prev, myend-prev);
-		
-		return ret;
 	}
 	
-	array<string> string::split(const string& sep, size_t limit) const
-	{
-		array<string> ret;
-		const uint8_t * data = ptr();
-		const uint8_t * dataend = ptr()+length();
-		
-		while (ret.size() < limit)
-		{
-			const uint8_t * next = (uint8_t*)memmem(data, dataend-data, sep.ptr(), sep.length());
-			if (!next) break;
-			ret.append(arrayview<uint8_t>(data, next-data));
-			data = next+sep.length();
-		}
-		ret.append(arrayview<uint8_t>(data, dataend-data));
-		return ret;
-	}
+	string ret;
+	uint8_t* retptr = ret.construct(outlen).ptr();
 	
-	array<string> string::rsplit(const string& sep, size_t limit) const
+	uint8_t* prev = ptr();
+	uint8_t* myend = ptr()+length();
+	while (true)
 	{
-		array<string> ret;
-		const uint8_t * datastart = ptr();
-		const uint8_t * data = ptr()+length();
+		uint8_t* match = (uint8_t*)memmem(prev, myend-prev, in.ptr(), in.length());
+		if (!match) break;
 		
-		const uint8_t * sepp = sep.ptr();
-		size_t sepl = sep.length();
+		memcpy(retptr, prev, match-prev);
+		retptr += match-prev;
+		prev = match + in.length();
 		
-		while (ret.size() < limit)
-		{
-			if (datastart+sepl > data) break;
-			const uint8_t * next = data-sepl;
-			while (memcmp(next, sepp, sepl)!=0)
-			{
-				if (datastart==next) goto done;
-				next--;
-			}
-			ret.insert(0, arrayview<uint8_t>(next+sepl, data-(next+sepl)));
-			data = next;
-		}
-	done:
-		ret.insert(0, arrayview<uint8_t>(datastart, data-datastart));
-		return ret;
+		memcpy(retptr, out.ptr(), out.length());
+		retptr += out.length();
 	}
+	memcpy(retptr, prev, myend-prev);
 	
-	string string::codepoint(uint32_t cp)
+	return ret;
+}
+
+array<string> string::split(const string& sep, size_t limit) const
+{
+	array<string> ret;
+	const uint8_t * data = ptr();
+	const uint8_t * dataend = ptr()+length();
+	
+	while (ret.size() < limit)
 	{
-		string ret;
-		if (cp<=0x7F)
-		{
-			ret[0] = cp;
-		}
-		else if (cp<=0x07FF)
-		{
-			ret[0] = (((cp>> 6)     )|0xC0);
-			ret[1] = (((cp    )&0x3F)|0x80);
-		}
-		else if (cp>=0xD800 && cp<=0xDFFF) return "\xEF\xBF\xBD";
-		else if (cp<=0xFFFF)
-		{
-			ret[0] = (((cp>>12)&0x0F)|0xE0);
-			ret[1] = (((cp>>6 )&0x3F)|0x80);
-			ret[2] = (((cp    )&0x3F)|0x80);
-		}
-		else if (cp<=0x10FFFF)
-		{
-			ret[0] = (((cp>>18)&0x07)|0xF0);
-			ret[1] = (((cp>>12)&0x3F)|0x80);
-			ret[2] = (((cp>>6 )&0x3F)|0x80);
-			ret[3] = (((cp    )&0x3F)|0x80);
-		}
-		else return "\xEF\xBF\xBD";
-		return ret;
+		const uint8_t * next = (uint8_t*)memmem(data, dataend-data, sep.ptr(), sep.length());
+		if (!next) break;
+		ret.append(arrayview<uint8_t>(data, next-data));
+		data = next+sep.length();
 	}
+	ret.append(arrayview<uint8_t>(data, dataend-data));
+	return ret;
+}
+
+array<string> string::rsplit(const string& sep, size_t limit) const
+{
+	array<string> ret;
+	const uint8_t * datastart = ptr();
+	const uint8_t * data = ptr()+length();
+	
+	const uint8_t * sepp = sep.ptr();
+	size_t sepl = sep.length();
+	
+	while (ret.size() < limit)
+	{
+		if (datastart+sepl > data) break;
+		const uint8_t * next = data-sepl;
+		while (memcmp(next, sepp, sepl)!=0)
+		{
+			if (datastart==next) goto done;
+			next--;
+		}
+		ret.insert(0, arrayview<uint8_t>(next+sepl, data-(next+sepl)));
+		data = next;
+	}
+done:
+	ret.insert(0, arrayview<uint8_t>(datastart, data-datastart));
+	return ret;
+}
+
+string string::codepoint(uint32_t cp)
+{
+	string ret;
+	if (cp<=0x7F)
+	{
+		ret[0] = cp;
+	}
+	else if (cp<=0x07FF)
+	{
+		ret[0] = (((cp>> 6)     )|0xC0);
+		ret[1] = (((cp    )&0x3F)|0x80);
+	}
+	else if (cp>=0xD800 && cp<=0xDFFF) return "\xEF\xBF\xBD";
+	else if (cp<=0xFFFF)
+	{
+		ret[0] = (((cp>>12)&0x0F)|0xE0);
+		ret[1] = (((cp>>6 )&0x3F)|0x80);
+		ret[2] = (((cp    )&0x3F)|0x80);
+	}
+	else if (cp<=0x10FFFF)
+	{
+		ret[0] = (((cp>>18)&0x07)|0xF0);
+		ret[1] = (((cp>>12)&0x3F)|0x80);
+		ret[2] = (((cp>>6 )&0x3F)|0x80);
+		ret[3] = (((cp    )&0x3F)|0x80);
+	}
+	else return "\xEF\xBF\xBD";
+	return ret;
+}
 
 test()
 {
