6c554afb0c0e244b49150ba1aa3049bc4bffddf4
Alcaro <floating@muncher.se>
2017-07-05 03:42:42 +0200

    Wonder if it optimizes better without those mallocs...

--- arlib/linq.h
+++ arlib/linq.h
@@ -3,19 +3,31 @@
 #ifndef LINQ_BASE_INCLUDED
 #define LINQ_BASE_INCLUDED
 
-template<typename T> class linqobj;
+template<typename T, typename Titer> class linqobj;
 //'T' is whatever the container contains.
-//'base' is the base class with .begin() and .end(), including template argument.
-//For example: template<typename T> class arrayview : public linqbase<T, arrayview<T>>
-template<typename T, typename base>
+//'Tbase' is the base class with .begin() and .end(), including template argument.
+//'Titer' is the type of .begin().
+//'Titer2' is the type of .end(), if different from .begin().
+//For example: template<typename T> class arrayview : public linqbase<T, arrayview<T>, const T*>
+template<typename T, typename Tbase>
 class linqbase : empty {
-	base& impl() { return *(base*)this; }
-	const base& impl() const { return *(base*)this; }
+	//doesn't exist, only used because the real impl() needs a 'this' and decltype doesn't have that
+	//dummy template parameters are to ensure it doesn't refer to Tbase::begin() before Tbase is properly defined
+	template<typename _> static Tbase& decltype_impl();
+	
+	Tbase& impl() { return *(Tbase*)this; }
+	const Tbase& impl() const { return *(Tbase*)this; }
+	
+	template<typename _>
+	linqobj<T, decltype(decltype_impl<_>().begin())> linq() const
+	{
+		return linqobj<T, decltype(impl().begin())>(impl().begin(), impl().end());
+	}
 public:
 	template<typename T3, typename T2 = typename std::result_of<T3(T)>::type>
-	linqobj<T2> select(T3 conv) const
+	auto select(T3 conv) const
 	{
-		return linqobj<T>(impl()).select(conv);
+		return linq<void>().select(conv);
 	}
 };
 #endif
@@ -27,100 +39,50 @@ public:
 #include "set.h"
 
 //This entire class is private. Do not store or create any instance, other than what the LINQ functions return.
-//Don't try to use it more than once, either. If you want to store it, cast to array.
-template<typename T> class linqobj : nocopy {
+template<typename T, typename Titer> class linqobj : nocopy {
 public:
-	class iterator : nocopy {
+	//TODO: split to two types when switching to c++17
+	Titer b;
+	Titer e;
+	
+	linqobj(Titer b, Titer e) : b(b), e(e) {}
+	
+	Titer begin() { return b; }
+	Titer end() { return e; }
+	
+	
+	template<typename Tout, typename Tconv>
+	class i_select {
 	public:
-		virtual bool operator!=(const iterator& other) = 0;
-		virtual void operator++() = 0;
-		virtual T operator*() = 0;
-		virtual ~iterator() {}
-	};
-	autoptr<iterator> it;
-	
-	linqobj(iterator* it) : it(it) {}
-	linqobj(autoptr<iterator> it) : it(it) {}
-	
-	virtual bool operator!=(const linqobj& other) { return *it != *it; }
-	virtual void operator++() { ++*it; }
-	virtual T operator*() { return **it; }
-	
-	
-	
-	class it_arrayview : public iterator {
-	public:
-		const T* it;
-		const T* end;
-		it_arrayview(arrayview<T> list) : it(list.begin()), end(list.end()) {}
-		bool operator!=(const iterator& other) { return it != end; }
-		void operator++() { ++it; }
-		T operator*() { return *it; }
-	};
-	linqobj(arrayview<T> list)
-	{
-		it = new it_arrayview(list);
-	}
-	
-	
-	class it_set : public iterator {
-	public:
-		typename set<T>::iterator it;
-		typename set<T>::iterator end;
-		it_set(const set<T>& s) : it(s.begin()), end(s.end()) {}
-		bool operator!=(const iterator& other) { return it != end; }
-		void operator++() { ++it; }
-		T operator*() { return *it; }
-	};
-	linqobj(const set<T>& list)
-	{
-		it = new it_set(list);
-	}
-	
-	
-	template<typename Tin, typename Tconv>
-	class it_select : public iterator {
-	public:
-		autoptr<typename linqobj<Tin>::iterator> base;
+		Titer base;
 		Tconv conv;
 		
-		it_select(typename linqobj<Tin>::iterator* base, Tconv conv) : base(base), conv(conv) {}
-		bool operator!=(const iterator& other) { return *base != *base; }
-		void operator++() { ++*base; }
-		T operator*() { return conv(**base); }
+		i_select(Titer base, Tconv conv) : base(base), conv(conv) {}
+		bool operator!=(const i_select other) { return base != other.base; }
+		void operator++() { ++base; }
+		Tout operator*() { return conv(*base); }
 	};
 	template<typename T3, typename T2 = typename std::result_of<T3(T)>::type>
-	linqobj<T2> select(T3 conv)
+	auto select(T3 conv)
 	{
-		return new typename linqobj<T2>::template it_select<T, T3>(it.release(), conv);
+		return linqobj<T2, i_select<T2, T3>>(i_select<T2, T3>(std::move(b), conv),
+		                                     i_select<T2, T3>(std::move(e), conv));
 	}
 	
 	
 	operator array<T>()
 	{
 		array<T> ret;
-		while (*it != *it)
-		{
-			ret.append(**it);
-			++*it;
-		}
+		for (auto&& item : *this) ret.append(item);
 		return ret;
 	}
 	
 	operator set<T>()
 	{
 		set<T> ret;
-		while (*it != *it)
-		{
-			ret.add(**it);
-			++*it;
-		}
+		for (auto&& item : *this) ret.add(item);
 		return ret;
 	}
-	
-	
-	linqobj begin() { return linqobj(it.release()); }
-	linqobj end() { return linqobj(NULL); }
 };
 
 #endif
